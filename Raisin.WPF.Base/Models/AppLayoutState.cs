beac8d55eb70ca41fab8e19c31a7fc36a10d0482
gerleim <gerleim@gmail.com>
2026-06-19 16:58:22 +0200

    Remove Documents property from AppLayoutState

--- Raisin.WPF.Base/Models/AppLayoutState.cs
+++ Raisin.WPF.Base/Models/AppLayoutState.cs
@@ -5,7 +5,6 @@ public record FloatingWindowBounds(double Left, double Top, double Width, double
 public class AppLayoutState
 {
     public int Version { get; set; } = 1;
-    public List<DocumentState> Documents { get; set; } = [];
     public double? WindowLeft { get; set; }
     public double? WindowTop { get; set; }
     public double? WindowWidth { get; set; }
