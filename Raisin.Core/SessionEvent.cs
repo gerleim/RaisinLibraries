3cb36a1ed1e75e0f9bcdb9a294020192d3be3d40
gerleim <gerleim@gmail.com>
2026-06-24 22:13:12 +0200

    Add account snapshot fields to SessionEvent
    Optional Account, NetLiquidation, PreviousDayEquity properties for
    daily account snapshots used by the trading report % gain feature.

--- Raisin.Core/SessionEvent.cs
+++ Raisin.Core/SessionEvent.cs
@@ -5,4 +5,7 @@ public class SessionEvent
     public DateTime Timestamp { get; init; }
     public string Type { get; init; } = "";
     public string? Detail { get; init; }
+    public string? Account { get; init; }
+    public double? NetLiquidation { get; init; }
+    public double? PreviousDayEquity { get; init; }
 }
